d2b94e9e9a9572d6f3011ab24e5f92d5d24af17a
patriciaSR <patricia.suarez.psr@gmail.com>
2020-02-19 14:58:47 +0100

    add signup component initial test

--- client/src/app/auth/signup/signup.component.spec.ts
+++ client/src/app/auth/signup/signup.component.spec.ts
@@ -1,25 +1,32 @@
-// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { SignupComponent } from './signup.component';
 
-// import { SignupComponent } from './signup.component';
+const apiConfig = {
+  api: 'https://localhost:3443'
+};
 
-// describe('SignupComponent', () => {
-//   let component: SignupComponent;
-//   let fixture: ComponentFixture<SignupComponent>;
+describe('SignupComponent', () => {
+  let component: SignupComponent;
+  let fixture: ComponentFixture<SignupComponent>;
 
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [ SignupComponent ]
-//     })
-//     .compileComponents();
-//   }));
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule, RouterTestingModule],
+      declarations: [ SignupComponent ],
+      providers: [{ provide: 'apiConfig', useValue: apiConfig }]
+    })
+    .compileComponents();
+  }));
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(SignupComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SignupComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
