a3d9eacde0715dfaf2b3424a7b1bd7c5f97aba61
patriciaSR <patricia.suarez.psr@gmail.com>
2020-02-20 18:27:48 +0100

    fix post detail testing activated route

--- client/src/app/posts/post-detail/post-detail.component.spec.ts
+++ client/src/app/posts/post-detail/post-detail.component.spec.ts
@@ -1,12 +1,13 @@
-// import { HttpClientTestingModule } from '@angular/common/http/testing';
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
-// import { ActivatedRoute } from '@angular/router';
-// import { RouterTestingModule } from '@angular/router/testing';
-// import { PostDetailComponent } from './post-detail.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ComponentFixture, inject, TestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { PostsStoreService } from '../posts-store.service';
+import { PostDetailComponent } from './post-detail.component';
 
-// const apiConfig = {
-//   api: 'https://localhost:3443'
-// };
+const apiConfig = {
+  api: 'https://localhost:3443'
+};
 
 // const routes = [
 //   {
@@ -21,37 +22,48 @@
 //   }
 // };
 
-// describe('PostDetailComponent', () => {
-//   let component: PostDetailComponent;
-//   let fixture: ComponentFixture<PostDetailComponent>;
+describe('PostDetailComponent', () => {
+  let component: PostDetailComponent;
+  let fixture: ComponentFixture<PostDetailComponent>;
 
-//   beforeEach((() => {
-//     TestBed.configureTestingModule({
-//       declarations: [PostDetailComponent],
-//       imports: [HttpClientTestingModule, RouterTestingModule.withRoutes(routes)],
-//       providers: [
-//         { provide: 'apiConfig', useValue: apiConfig },
-//         { provide: ActivatedRoute,  useValue: fakeActivatedRoute },
-//       ]
-//     })
-//       .compileComponents();
-//   }));
+  beforeEach((() => {
+    TestBed.configureTestingModule({
+      declarations: [PostDetailComponent],
+      imports: [HttpClientTestingModule, RouterTestingModule],
+      providers: [
+        { provide: 'apiConfig', useValue: apiConfig }
+      ]
+    })
+      .compileComponents();
+  }));
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(PostDetailComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PostDetailComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
 
-//   it('should navigate to newpost path', inject([Router], (router: Router) => {
-//     const spyNav = spyOn(router, 'navigate');
+  it('should navigate to newpost path', inject([Router], (router: Router) => {
+    const spyNav = spyOn(router, 'navigate');
 
-//     component.onNavigate('newpost', '');
+    component.onNavigate('newpost', '');
 
-//     expect(spyNav).toHaveBeenCalledWith(['newpost']);
-//  }));
-// });
+    expect(spyNav).toHaveBeenCalledWith(['newpost']);
+  }));
+
+  it('should call deletePost service on onDelete() method', inject([Router], async (router: Router) => {
+    const postID = '5e31d951bcdbf849883e9bd0';
+    const spyService = spyOn(TestBed.inject(PostsStoreService), 'deletePost$');
+
+    const spyNav = spyOn(router, 'navigate');
+
+    await component.onDelete(postID);
+
+    expect(spyService).toHaveBeenCalled();
+    expect(spyNav).toHaveBeenCalledWith(['posts']);
+  }));
+});
