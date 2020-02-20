0caa6a709a050680a3d6cccd321cacc5efcbff62
patriciaSR <patricia.suarez.psr@gmail.com>
2020-02-20 12:22:43 +0100

    add post-store tests

--- client/src/app/posts/posts-proxy.service.spec.ts
+++ client/src/app/posts/posts-proxy.service.spec.ts
@@ -1,5 +1,6 @@
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { FAKE_POST } from './post-fake.spec';
 import { FAKE_POSTS } from './posts-fake.spec';
 import { PostsProxyService } from './posts-proxy.service';
 
@@ -7,9 +8,13 @@ const apiConfig = {
   api: 'https://localhost:3443'
 };
 
-
 describe('PostsProxyService', () => {
   let service: PostsProxyService;
+
+  const newPost = {
+    title: 'hola soy dumbo111',
+    content: 'hola soy dumbo111'
+  };
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -35,15 +40,39 @@ describe('PostsProxyService', () => {
     httpMock.verify();
   });
 
-  // it('should call to server to get all posts', () => {
-  //   // const service: UsersProxyService = TestBed.get(UsersProxyService);
-  //   const httpMock = TestBed.inject(HttpTestingController);
-  //   service.getPosts().subscribe(posts => expect(posts).toBe(FAKE_POSTS));
+  it('should call to server to get post by id', () => {
+    const id = '5e31d951bcdbf849883e9bd0';
+    const httpMock = TestBed.inject(HttpTestingController);
+    service.getPostById(id).subscribe(posts => expect(posts).toBe(FAKE_POST));
 
-  //   const request = httpMock.expectOne('https://localhost:3443/posts');
+    const request = httpMock.expectOne('https://localhost:3443/posts/' + id);
 
-  //   expect(request.request.method).toEqual('GET');
-  //   request.flush(FAKE_POSTS);
-  //   httpMock.verify();
-  // });
+    expect(request.request.method).toEqual('GET');
+    request.flush(FAKE_POST);
+    httpMock.verify();
+  });
+
+  it('should call to server to send new post', () => {
+    const httpMock = TestBed.inject(HttpTestingController);
+    service.sendPost(newPost).subscribe(post => expect(post.title).toBe(FAKE_POST.title));
+
+    const request = httpMock.expectOne('https://localhost:3443/posts');
+
+    expect(request.request.method).toEqual('POST');
+    request.flush(FAKE_POST);
+    httpMock.verify();
+  });
+
+  it('should call to server to delete post by id', () => {
+    const httpMock = TestBed.inject(HttpTestingController);
+    const id = '5e4e61277b3414281ef448ad';
+
+    service.deletePost(id).subscribe(post => expect(post).toBe(FAKE_POST));
+
+    const request = httpMock.expectOne('https://localhost:3443/posts/' + id);
+
+    expect(request.request.method).toEqual('DELETE');
+    request.flush(FAKE_POST);
+    httpMock.verify();
+  });
 });
--- client/src/app/posts/posts-store.service.spec.ts
+++ client/src/app/posts/posts-store.service.spec.ts
@@ -1,6 +1,10 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
+import { Store } from '../state/store';
+import { FAKE_POSTS } from './posts-fake.spec';
 import { PostsStoreService } from './posts-store.service';
+import { PostsService } from './posts.service';
 
 const apiConfig = {
   api: 'https://localhost:3443'
@@ -9,10 +13,15 @@ const apiConfig = {
 describe('PostsStoreService', () => {
   let service: PostsStoreService;
 
+  const newPost = {
+    title: 'hola soy dumbo111',
+    content: 'hola soy dumbo111'
+  };
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
-      providers: [{ provide: 'apiConfig', useValue: apiConfig }]
+      providers: [{ provide: 'apiConfig', useValue: apiConfig }, Store]
     });
     service = TestBed.inject(PostsStoreService);
   });
@@ -20,4 +29,31 @@ describe('PostsStoreService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should return a Promise<Post[]> on init()', async () => {
+    const spyService = spyOn(TestBed.inject(PostsService), 'getPosts').and.callFake(() => of(FAKE_POSTS));
+
+    await service.init();
+
+    expect(spyService).toHaveBeenCalled();
+  });
+
+  // it('should return a Promise<PostDTO> on createPost()', async () => {
+  //   const spyService = spyOn(TestBed.inject(PostsService), 'createPost').and.callFake(() => of(FAKE_POST));
+  //   const spyStore = spyOn(TestBed.inject(Store), 'get').and.callFake(() => FAKE_POSTS);
+
+  //   await service.createPost$(newPost);
+
+  //   expect(spyStore).toHaveBeenCalled();
+  //   expect(spyService).toHaveBeenCalled();
+  // });
+
+  // it('should return a Promise<PostDTO> on deletePost()', async () => {
+  //   const spyService = spyOn(TestBed.inject(PostsService), 'deletePost').and.callFake(() => of(FAKE_POST));
+  //   const id = '5e31d951bcdbf849883e9bd0';
+
+  //   await service.deletePost$(id);
+
+  //   expect(spyService).toHaveBeenCalled();
+  // });
 });
