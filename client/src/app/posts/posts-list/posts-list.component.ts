183f4b7f47584e415e00a888fdcae108c00bbb02
patriciaSR <patricia.suarez.psr@gmail.com>
2020-02-20 12:30:58 +0100

    add delete button to post list

--- client/src/app/posts/posts-list/posts-list.component.ts
+++ client/src/app/posts/posts-list/posts-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { Post } from '../post.model ';
 import { PostsStoreService } from '../posts-store.service';
@@ -11,10 +12,22 @@ import { PostsStoreService } from '../posts-store.service';
 export class PostsListComponent implements OnInit {
   posts$: Observable<Post[]>;
 
-  constructor(private store: PostsStoreService) { }
+  constructor(private store: PostsStoreService, private router: Router) { }
 
   ngOnInit(): void {
     this.store.init();
     this.posts$ = this.store.get$();
+  }
+
+  onNavigate(path: string) {
+    this.router.navigate([path]);
+  }
+
+  async onDelete(postID: string) {
+    try {
+      await this.store.deletePost$(postID);
+    } catch (error) {
+      console.log(error);
+    }
   }
 }
